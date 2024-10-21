c0024e08fdfbcef32701a80c078661de36e00f49
0xjessel <github@jessechen.net>
2024-10-20 17:08:47 -0700

    the bot is not picking up on some earthquakes, i think its because they retroactively add in earthquakes when they are reviewed so the start/end timestamps will filter those out.  instead use updatedafter which i think will pick up all new earthquakes as they come in

--- fetch_and_post_earthquake.py
+++ fetch_and_post_earthquake.py
@@ -19,8 +19,7 @@ def fetch_new_earthquakes():
         try:
             params = {
                 'format': 'geojson',
-                'starttime': (current_time - timedelta(minutes=5)).isoformat(),  
-                'endtime': current_time.isoformat(),  
+                'updatedafter': (current_time - timedelta(minutes=5)).isoformat(),  
                 'minmagnitude': 5.0,
             }
 
@@ -36,7 +35,7 @@ def fetch_new_earthquakes():
                     print(f"found a non-earthquake type: {feature['properties']['type']}")
                     continue  
 
-                print(f"found earthquake at time: {feature['properties']['time']}")
+                print(f"found earthquake at time: {feature['properties']['updated']}")
                 new_earthquakes.append(feature)
             
             return new_earthquakes
